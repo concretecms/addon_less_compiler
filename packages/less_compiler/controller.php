d35499e0e3aae995ac81221f2d2609960de90dfb
Korvin Szanto <korvinszanto@gmail.com>
2013-03-21 10:22:20 -0700

    Update to v 1.2

--- packages/less_compiler/controller.php
+++ packages/less_compiler/controller.php
@@ -4,7 +4,7 @@ class LessCompilerPackage extends Package {
 
 	protected $pkgHandle = 'less_compiler';
 	protected $appVersionRequired = '5.5';
-	protected $pkgVersion = '1.1';
+	protected $pkgVersion = '1.2';
 
 	public function getPackageDescription() {
 		return t('Compile your Less with a job.');
